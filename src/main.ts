36ba0faff15eb5b3c4de146a2805c929038fae77
Tomasz Godzik <tgodzik@virtuslab.com>
2026-02-06 10:05:52 +0100

    improvement: Download bloop to work around Scala CLI issue

--- src/main.ts
+++ src/main.ts
@@ -143,6 +143,10 @@ async function run(): Promise<void> {
       }
     })
 
+    await core.group('Download Bloop', async () => {
+      await cs('fetch', 'ch.epfl.scala:bloop-frontend_2.12:2.0.17')
+    })
+
     await core.group('Install Apps', async () => {
       const value = core.getInput('apps').trim()
       const apps: string[] = value.split(' ')
